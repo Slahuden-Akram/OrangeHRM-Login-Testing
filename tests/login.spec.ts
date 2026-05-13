3e35dca1f878094eaa6a7ad84e9fb252e4d68b35
Slahuden Akram <slahudenakram0@gmail.com>
2026-05-13 22:30:17 +0100

    credential test cases added

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from '@playwright/test';
 
 const correctUsername = 'Admin';
 const correctPassword = 'admin123';
-
+const invalidUsername = 'invalid';
+const invalidPassword = 'password';
 const loginPage = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login';
 test.describe("Login", () => {
     test.beforeEach("Login page should load", async ({ page }) => {
@@ -27,4 +28,35 @@ test.describe("Login", () => {
         await page.locator('button[type="submit"]').click();
         await expect(page).toHaveURL(/dashboard/);
     });
+    test('Invalid credentials should appear on invalid username', async ({ page }) => {
+        await page.locator('input[name="username"]').fill(invalidUsername);
+        await page.locator('input[name="password"]').fill(correctPassword);
+        await page.locator('button[type="submit"]').click();
+        await expect(page.getByText('Invalid credentials')).toBeVisible();
+    });
+    test('Invalid credentials should appear on invalid password', async ({ page }) => {
+        await page.locator('input[name="username"]').fill(correctUsername);
+        await page.locator('input[name="password"]').fill(invalidPassword);
+        await page.locator('button[type="submit"]').click();
+        await expect(page.getByText('Invalid credentials')).toBeVisible();
+    });
+    test('Required should appear on Empty Fields', async ({ page }) => {
+        await page.locator('input[name="username"]').fill('');
+        await page.locator('input[name="password"]').fill('');
+        await page.locator('button[type="submit"]').click();
+        await expect(page.getByText("Required")).toHaveCount(2);
+    });
+    test('Required should appear when user enter password only', async ({ page }) => {
+        await page.locator('input[name="username"]').fill('');
+        await page.locator('input[name="password"]').fill(correctPassword);
+        await page.locator('button[type="submit"]').click();
+        await expect(page.getByText('Required').first()).toBeVisible();
+    });
+    test('Required should appear when user enter username only', async ({ page }) => {
+        await page.locator('input[name="username"]').fill(correctUsername);
+        await page.locator('input[name="password"]').fill('');
+        await page.locator('button[type="submit"]').click();
+        await expect(page.getByText('Required').last()).toBeVisible();
+    });
+
 });
